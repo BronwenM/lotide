1118644760a04a600d749058b3357e3f1e0e8530
BronwenM <bronwen.marchant@gmail.com>
2024-09-18 16:20:13 -0700

    linted middle.js

--- middle.js
+++ middle.js
@@ -1,36 +1,37 @@
 const eqArrays = (a, b) => {
-    let arrsMatch = true; //default value is arrays do not match
+  let arrsMatch = true; //default value is arrays do not match
 
-    if (a.length === b.length) { //make sure the arrays have the same length, otherwise auto-fails
-        for (let index in a) {
-            if (a[index] !== b[index]) { arrsMatch = false; break; }
-        }
+  if (a.length === b.length) { //make sure the arrays have the same length, otherwise auto-fails
+    for (let index in a) {
+      if (a[index] !== b[index]) {
+        arrsMatch = false; break;
+      }
     }
-    else arrsMatch = false;
+  } else arrsMatch = false;
 
-    return arrsMatch;
+  return arrsMatch;
 };
 
 const assertArraysEqual = (arrOne, arrTwo) => {
-    if (eqArrays(arrOne, arrTwo)) console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
-    else console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
-}
+  if (eqArrays(arrOne, arrTwo)) console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  else console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+};
 
 const middle = (arr) => {
-    let midArr = [];
-    let midIndex;
+  let midArr = [];
+  let midIndex;
 
-    if (arr.length > 2) {
-        midIndex = (arr.length / 2);
+  if (arr.length > 2) {
+    midIndex = (arr.length / 2);
 
-        if (arr.length % 2 !== 0) midArr.push(arr[Math.floor(midIndex)]);
-        else midArr.push(arr[midIndex - 1], arr[midIndex])
+    if (arr.length % 2 !== 0) midArr.push(arr[Math.floor(midIndex)]);
+    else midArr.push(arr[midIndex - 1], arr[midIndex]);
 
 
-        return midArr;
-    }
-    return []
-}
+    return midArr;
+  }
+  return [];
+};
 
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
