ec96baa6327d235251e2d17e915544f880b6eb6b
BronwenM <bronwen.marchant@gmail.com>
2024-09-23 16:47:26 -0700

    Moved middleTest.js tests to Chai assert statements

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), [6]);
-assertArraysEqual(middle([1, 2]), []);
+describe("#tail", () => {
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+    });
+
+    it("returns [6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), [6]); 
+    });
+
+    it("returns [] for [1, 2]", () => {
+        assert.deepEqual(middle([1, 2]), []); 
+    });
+})
