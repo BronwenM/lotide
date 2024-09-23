f77c941d7feeee585bbffaacfccb52db19ab1b8d
BronwenM <bronwen.marchant@gmail.com>
2024-09-23 16:48:49 -0700

    Typo fix in middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,7 @@
 const middle = require("../middle");
 const assert = require("chai").assert;
 
-describe("#tail", () => {
+describe("#middle", () => {
     it("returns [2, 3] for [1, 2, 3, 4]", () => {
         assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
     });
